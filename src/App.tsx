c90da067247a9311c64459493a711cca1e238175
akumbom <akumbom5ma@gmail.com>
2022-11-09 15:56:43 +0100

    designed game board

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import { useState, useEffect} from "react";
+
+const width = 8;
+const candyColor = [
+  'blue',
+  'green',
+  'orange',
+  'purple',
+  'red',
+  'yellow'
+]
+
 
 function App() {
+
+  const [currentColorArr, setCurrentColorArr] = useState<any>([]);
+
+  const checkForColumnsOfThree = () => {
+    for(let i = 0; i < 47; i++) {
+      const columnOfThree = [i, i + width, i + width * 2]
+      const decidedColor = currentColorArr[i];
+
+      if ( columnOfThree.every(square => currentColorArr[square] === decidedColor)){
+         columnOfThree.forEach(element => {
+           currentColorArr[i] = ''
+         });
+      }
+    }
+  }
+
+ const createBoard = () => {
+  const randomColorArrangement = [];
+   for(let i = 0 ; i < width * width; i++){
+    const randomColor = candyColor[Math.floor(Math.random() * candyColor.length)]
+    randomColorArrangement.push(randomColor)
+   }
+   setCurrentColorArr(randomColorArrangement)
+ }
+ useEffect(() => {
+      createBoard()
+ }, [])
+ 
+ useEffect(() => {
+  const timer =  setInterval(() => {
+    checkForColumnsOfThree()
+    setCurrentColorArr([...currentColorArr])
+  }, 100)
+  return () => clearInterval(timer)
+
+ }, [checkForColumnsOfThree, currentColorArr])
+ 
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">   
+      <div className="game">
+         {currentColorArr.map((candyColor: string, index : number) => {
+            return(
+              <img 
+              key={index}
+              style={{backgroundColor: candyColor}}
+              />
+            )
+         })}
+        </div>     
     </div>
   );
 }
